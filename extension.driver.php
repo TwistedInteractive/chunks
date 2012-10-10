9372b57e3bb0ca690b921473e438871d2209a1ac
Giel Berkers <giel@twisted.nl>
2012-10-10 12:46:24 +0200

    Update to 1.1.0

--- extension.driver.php
+++ extension.driver.php
@@ -13,13 +13,23 @@
 					'page' => '/backend/',
 					'delegate' => 'InitaliseAdminPageHead',
 					'callback' => 'modifyHeader'
+				),
+				array(
+					'page'		=> '/system/preferences/',
+					'delegate'	=> 'AddCustomPreferenceFieldsets',
+					'callback'	=> 'actionAddCustomPreferenceFieldsets'
+				),
+				array(
+					'page'		=> '/system/preferences/',
+					'delegate'	=> 'Save',
+					'callback'	=> 'actionSave'
 				)
 			);
 		}
 
 		public function addParameter($context)
 		{
-			if(Frontend::instance()->isLoggedIn())
+			if(Frontend::instance()->isLoggedIn() && Symphony::Configuration()->get('disabled', 'chunks') != 'yes')
 			{
 				$context['params']['chunks-logged-in'] = 'yes';
 			} else {
@@ -33,6 +43,57 @@
 			{
 				Administration::instance()->Page->addStylesheetToHead(URL.'/extensions/chunks/assets/chunks.css');
 				Administration::instance()->Page->addScriptToHead(URL.'/extensions/chunks/assets/chunks.js');
+			}
+		}
+
+		/*
+		 * Delegate 'AddCustomPreferenceFieldsets' function
+		 * @param $context
+		 *  Provides the following parameters:
+		 *  - wrapper (XMLElement) : An XMLElement of the current page
+		 *  - errors (array) : An array of errors
+		 */
+		public function actionAddCustomPreferenceFieldsets($context)
+		{
+			// Create preference group
+			$group = new XMLElement('fieldset');
+			$group->setAttribute('class', 'settings');
+			$group->appendChild(new XMLElement('legend', __('Chunks')));
+
+			$label = Widget::Label();
+			$value = Symphony::Configuration()->get('disabled', 'chunks');
+			if(empty($value)) { $value = 'no'; }
+			$input = Widget::Input('settings[chunks][disabled]', 'yes' , 'checkbox', ($value == 'yes' ? array('checked'=>'checked') : null));
+			$label->setValue($input->generate() . ' ' . __('Disable edit-boxes for authors.'));
+			$group->appendChild($label);
+
+			// Append new preference group
+			$context['wrapper']->appendChild($group);
+		}
+
+		/*
+		 * Delegate 'Save' function
+		 * @param $context
+		 *  Provides the following parameters:
+		 *  - settings (array) : An array of the preferences to be saved, passed by reference
+		 *  - errors (array) : An array of errors passed by reference
+		 */
+		public function actionSave($context)
+		{
+			// Save the configuration
+			$data = $context['settings']['chunks'];
+			if(!isset($data['disabled'])) { $data['disabled'] = 'no'; }
+			foreach($data as $key => $value)
+			{
+				Symphony::Configuration()->set($key, $value, 'chunks');
+			}
+			if(version_compare(Administration::Configuration()->get('version', 'symphony'), '2.2.5', '>'))
+			{
+				// S2.3+
+				Symphony::Configuration()->write();
+			} else {
+				// S2.2.5-
+				Administration::instance()->saveConfig();
 			}
 		}
 	}
